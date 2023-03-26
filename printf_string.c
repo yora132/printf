d49399cf1f162c6c28f6f4a8d2509ce0b2f6dfc6
yora132 <yarasalah132@gmail.com>
2023-03-26 10:48:30 -0700

    task0

---
+++ printf_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * printf_string - prints a string
+ * @args: input format
+ * Return: len
+ */
+int printf_string(va_list args)
+{
+	int len, i;
+	char *str;
+
+	str = va_arg(args, char *);
+
+	if (str != NULL)
+	{
+		len = _strlen(str);
+		for (i = 0; i < len; i++)
+			_putchar(str[i]);
+
+	}
+	return (len);
+}
+
+
+/**
+ * _strlen - Returns the lenght of a string.
+ * @str: Type char pointer
+ * Return: z.
+ */
+int _strlen(char *str)
+{
+	int z;
+
+	for (z = 0; str[z] != 0; z++)
+		;
+	return (z);
+
+}
