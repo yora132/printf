c16129ebc5bdbb576fe1db80bee637eb2b085b03
yora132 <yarasalah132@gmail.com>
2023-03-26 10:43:31 -0700

    task0

--- main.h
+++ main.h
@@ -1,9 +1,35 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <limits.h>
+
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+}convert_match;
+
+
+
+
 int _printf(const char *format, ...);
-int print_char(va_list args, int num);
-int print_string(va_list args, int num);
+int _putchar(char c);
+int printf_37(void);
+int _strlen(char *str);
+int printf_char(va_list args);
+int printf_string(va_list args);
 int print_integer(va_list args, int num);
+
+
 #endif
