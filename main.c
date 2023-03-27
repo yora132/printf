37c6a1f4284577088520b507228b0329df8fd59f
Waled Wael <waledwael0@gmail.com>
2023-03-27 08:33:10 -0700

    Task 1

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+int main(void)
+{
+	int len;
+ 	int len2;
+	len = _printf("Let's try to printf a simple sentence%d.\n", 25);
+    	len2 = printf("Let's try to printf a simple sentence%d.\n", 25);
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n");
+	printf("Negative:[%d]\n");
+        _printf("Number:[%d]\n", 0);
+        printf("Number:[%d]\n", 0);
+        _printf("Number:[%d]\n", 2147483647);
+        printf("Number:[%d]\n", 2147483647);
+        _printf("Number:[%d]\n", -2147483648);
+        printf("Number:[%d]\n", -2147483648);
+        _printf("Number:[%d]\n", 9876543210);
+        printf("Number:[%d]\n", 9876543210);
+        _printf("Number:[%d]\n", 001);
+        printf("Number:[%d]\n", 001);
+	_printf("Character:[%c]\n", 'H');
+   	printf("Character:[%c]\n", 'H');
+   	_printf("String:[%s]\n", "I am a string !");
+    	printf("String:[%s]\n", "I am a string !");
+	len = _printf("Percent:[%%]\n");
+   	len2 = printf("Percent:[%%]\n");
+    	_printf("Len:[%d]\n", len);
+    	printf("Len:[%d]\n", len2);
+	return (0);
+}
+
