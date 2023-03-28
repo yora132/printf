95da4d723559e7ec9ffaa52e09e08a1b612cd62b
yora132 <yarasalah132@gmail.com>
2023-03-28 10:34:44 -0700

    done

--- main.h
+++ main.h
@@ -22,6 +22,20 @@ typedef struct format
 } convert_match;
 
 
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
 
 
 int _printf(const char *format, ...);
